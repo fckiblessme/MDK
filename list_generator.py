d27c32930a43fbc467e6f1157a00402825e4ffe5
Viktoria <wcbrtidkw@gmail.com>
2026-02-20 01:28:07 +0300

    Add second version of list_generator.py

--- list_generator.py
+++ list_generator.py
@@ -1,6 +1,11 @@
 import random
 n = 18
 def create_list(count):
-	return [random.randint(5, n * 100) for i in range(count)]
+	result = []
+	i = 0
+	while i < count:
+		result.append(random.randint(5, n * 100))
+		i = i + 1
+	return result
 numbers = create_list(5)
 print(numbers)
